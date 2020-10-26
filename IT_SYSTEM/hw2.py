1d78136f603b76b0d5cf9a5116a6ffd04af58a60
Kiminje <sis013@naver.com>
2020-10-26 22:40:30 +0900

    ITsystem hw2.py is real

--- IT_SYSTEM/hw2.py
+++ IT_SYSTEM/hw2.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-import pandas as pd
-
-
-from enum import Enum as En
-
+from time import time
 cancer = load_breast_cancer()
 x = cancer.data
 y = cancer.target
@@ -24,22 +20,14 @@ PRINT_ANNEAL_EVERY = 30000
 ANNEAL = False
 Epochs = 20000
 
-BatchSize = 10
-
-
-# print(mode.BATCH)
-# MODE = mode.BATCH
-# print(MODE == 1)
-
-
 class LogisticNeuron:
 
     def __init__(self):
         self.w = None
         self.b = None
         self.eta = None
+        self.Score = []
         self.losses = []
-        self.Scores = []
         self.w_history = []
         self.b_history = []
         self.Datalen = None
@@ -56,8 +44,8 @@ class LogisticNeuron:
         # print("shape info", Ypartial.shape, Wpartial.shape, Zpartial.shape)
         seq1 = np.dot(Wpartial.T, Zpartial * Ypartial)
         # print(seq1.shape)
-        w_grad = seq1 / len(y)  # (dividing by num of data, )
-        b_grad = np.sum(Ypartial * Zpartial) / len(y)
+        w_grad = seq1 / self.Datalen   # (num of data, )
+        b_grad = np.sum(Ypartial * Zpartial) / self.Datalen
         # print(w_grad.shape, b_grad.shape)
         # w_grad_k = x_k * err_k  # 가중치에 대한 그래디언트를 계산합니다
         # b_grad_k = 1 * err_k  # 절편에 대한 그래디언트를 계산합니다
@@ -68,60 +56,90 @@ class LogisticNeuron:
         a_k = 1.0 / (1.0 + np.exp(-z_k))  # 시그모이드 계산
         return a_k
 
-    def fit(self, x, y, epochs=Epochs, Shuffle=False, Batch=False, Size=BatchSize):
+    def fit(self, x, y, epochs=Epochs, Shuffle=True):
+        start = time()
         self.w = np.ones(x.shape[1])  # 가중치를 초기화합니다.
         self.b = np.ones(1)  # 절편을 초기화합니다.
-        self.eta = 0.0001
+        self.eta = 0.001
         self.w_history.append(self.w.copy())
         self.b_history.append(self.b)
-        self.Datalen = len(y)
-        # print(self.Datalen)
+        self.Datalen = x.shape[0]
+        print(self.Datalen)
 
         for i in range(epochs):  # epochs만큼 반복합니다
-
-            if Shuffle == True:
-                x, y = shuffle(x, y)
-                if Batch:
-                    random_indices = np.random.choice(self.Datalen, size=Size, replace=False)
-                    y_s = y[random_indices]
-                    x_s = x[random_indices]
-                else:
-                    x_s, y_s = x, y
-            else:
-                x_s, y_s = x, y
-
-            w_grad = np.zeros(x_s.shape[1])
-            # print("wgradshape", w_grad.shape)
-            # print("x_s shape", x_s.shape)
-
+            w_grad = np.zeros(x.shape[1])
+            # print(w_grad.shape, self.w.shape)
             b_grad = 0.0
             loss = 0
+            if Shuffle == True:
+                x, y = shuffle(x, y)
+
+            z = self.forpass(x)  # 정방향 계산
+            a = self.activation(z)  # sigmoid
+            sub = y - a  # y - y_hat
+            loss = np.sum(-np.multiply(y, np.log(a))) / self.Datalen  # cross entropy
+            # print(self.w, self.b)
+            w_grad, b_grad = self.backprop(x, y, a)
+
+            self.w -= self.eta * w_grad  # 가중치 업데이트
+            self.b -= self.eta * b_grad  # 절편 업데이트
+            partial = self.score(x, y)
+            if i % PRINT_EVERY == 0:
+                if i % PRINT_ANNEAL_EVERY == 0 & i != 0:
+                    self.eta *= 0.5
+                    print("learning rate %4f" % self.eta)
+                print("iter : %d" %i, end='\t')
+                print("loss %5f" % loss, end='\t')
+                print("score: %4f" % partial)
+            self.losses.append(loss)
+            self.w_history.append(self.w.copy())
+            self.b_history.append(self.b)
+            self.Score.append(partial)
+        end = time() - start
+        print("executed time %3f" % end)
+
+    def batch_fit(self, x, y, epochs=Epochs, Shuffle=True, Size=30):
+        start = time()
+        self.w = np.ones(x.shape[1])  # 가중치를 초기화합니다.
+        self.b = np.ones(1)  # 절편을 초기화합니다.
+        self.eta = 0.001
+        self.w_history.append(self.w.copy())
+        self.b_history.append(self.b)
+        self.Datalen = Size
+
+        for i in range(epochs):  # epochs만큼 반복합니다
+            w_grad = np.zeros(x.shape[1])
+            # print(w_grad.shape, self.w.shape)
+            b_grad = 0.0
+            loss = 0
+            if Shuffle:
+                x, y = shuffle(x, y)
+
+            random_indices = np.random.choice(455, size=Size, replace=False)
+            y_s = y[random_indices]
+            x_s = x[random_indices]
+
             z = self.forpass(x_s)  # 정방향 계산
             a = self.activation(z)  # sigmoid
             sub = y_s - a  # y - y_hat
-            loss = np.sum(-np.multiply(y_s, np.log(a))) / len(y_s)  # cross entropy
-
+            loss = np.sum(-np.multiply(y_s, np.log(a))) / self.Datalen  # cross entropy
+            # print(self.w, self.b)
             w_grad, b_grad = self.backprop(x_s, y_s, a)
 
             self.w -= self.eta * w_grad  # 가중치 업데이트
             self.b -= self.eta * b_grad  # 절편 업데이트
-
-            partial_score = self.score(x, y)
-            self.Scores.append(partial_score)
             if i % PRINT_EVERY == 0:
-                print("shape is ", x_s.shape, y_s.shape)
-                if i % PRINT_ANNEAL_EVERY == 0:
+                if (i % PRINT_ANNEAL_EVERY == 0) & i !=0:
                     self.eta *= 0.5
                     print("learning rate %4f" % self.eta)
-                print("iter : %d" % i, end='\t')
+                print("iter : %d" %i, end='\t')
                 print("loss %5f" % loss, end='\t')
-                print("score: %4f" % partial_score)
-
-            # if i > 300:
+                print("score: %4f" % self.score(x, y))
             self.losses.append(loss)
-
             self.w_history.append(self.w.copy())
             self.b_history.append(self.b)
+        end = time() - start
+        print("executed time %3f" % end)
 
     def predict(self, x):
         z = self.forpass(x)  # 정방향 계산
@@ -136,17 +154,24 @@ class LogisticNeuron:
 # neuron.fit(x_train, y_train)
 # print(neuron.score(x_test, y_test))
 neuron_scaled = LogisticNeuron()
-neuron_scaled.fit(x_train_scaled, y_train)
+neuron_scaled.batch_fit(x_train_scaled, y_train, Size=1)
 print(neuron_scaled.score(x_test_scaled, y_test))
 
-neuron_scaled_shuffle = LogisticNeuron()
-neuron_scaled_shuffle.fit(x_train_scaled, y_train, Shuffle=True, Batch=True)
-print(neuron_scaled_shuffle.score(x_test_scaled, y_test))
 # plt.plot(neuron.losses, color='green')
 # plt.plot(neuron_scaled.losses)
-plt.plot(neuron_scaled.Scores, color='green')
-# plt.plot(neuron_scaled_shuffle.losses, color='red')
-plt.plot(neuron_scaled_shuffle.Scores, color='blue')
+
+neuron_scaled_batch = LogisticNeuron()
+neuron_scaled_batch.batch_fit(x_train_scaled, y_train, Size=50)
+print(neuron_scaled_batch.score(x_test_scaled, y_test))
+
+neuron_scaled_batch1 = LogisticNeuron()
+neuron_scaled_batch1.batch_fit(x_train_scaled, y_train, Size=455)
+print(neuron_scaled_batch1.score(x_test_scaled, y_test))
+
+# plt.plot(neuron.losses, color='green')
+plt.plot(neuron_scaled.losses, color='red')
+plt.plot(neuron_scaled_batch.losses, color='green')
+plt.plot(neuron_scaled_batch1.losses, color='blue')
 plt.xlabel('epoch')
 plt.ylabel('loss')
 plt.show()
